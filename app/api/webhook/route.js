ee96cf435683b85fdfb33eb6d691f7f503ea40a3
Moch. Hafizh Azi <hafizhreal05@gmail.com>
2026-05-24 01:40:52 +0700

    Update route.js

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -3,7 +3,28 @@ export async function POST(req) {
 
   console.log(body)
 
+  const from = body.from
+  const message = body.message
+
+  // kalau ada pesan masuk
+  if (from && message) {
+
+    // kirim balasan
+    await fetch("https://yilzi.me/api/send/message?device=1", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer zap_967f4e66eb7a8341488ecf0296d895ed"
+      },
+      body: JSON.stringify({
+        to: from,
+        message: `Halo 👋\nPesan kamu: ${message}`
+      })
+    })
+
+  }
+
   return Response.json({
     success: true
   })
-}
+                           }
